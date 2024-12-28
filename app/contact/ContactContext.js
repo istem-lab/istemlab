da2658bb865f06c30d86e4f416712db187a503b6
shriyaaaaaaaaa <shriyakhatri2004@gmail.com>
2024-12-28 22:00:55 +0545

    eslint error solve

--- app/contact/ContactContext.js
+++ app/contact/ContactContext.js
@@ -1,22 +1,27 @@
-"use client";
+'use client';
 
 import React, { createContext, useState } from 'react';
 
 export const ContactContext = createContext();
 
-const CustomAlert = ({ type, message, onClose }) => {
-  
-    return (
-      <Alert className={type === 'success' ? 'bg-green-200 text-green-900' : 'bg-red-200 text-red-900'} variant={type === 'success' ? 'success' : 'destructive'}>
-        <AlertTitle>{type === 'success' ? 'Success' : 'Error'}</AlertTitle>
-        <AlertDescription>{message}</AlertDescription>
-        <Button variant="ghost" size="icon" onClick={onClose}>
-          <X className="w-4 h-4" />
-        </Button>
-      </Alert>
-    );
-  };
-
+// const CustomAlert = ({ type, message, onClose }) => {
+//   return (
+//     <Alert
+//       className={
+//         type === 'success'
+//           ? 'bg-green-200 text-green-900'
+//           : 'bg-red-200 text-red-900'
+//       }
+//       variant={type === 'success' ? 'success' : 'destructive'}
+//     >
+//       <AlertTitle>{type === 'success' ? 'Success' : 'Error'}</AlertTitle>
+//       <AlertDescription>{message}</AlertDescription>
+//       <Button variant="ghost" size="icon" onClick={onClose}>
+//         <X className="w-4 h-4" />
+//       </Button>
+//     </Alert>
+//   );
+// };
 
 export const ContactProvider = ({ children }) => {
   const [loading, setLoading] = useState(false);
@@ -33,16 +38,22 @@ export const ContactProvider = ({ children }) => {
         },
         body: JSON.stringify(data),
       });
-      console.log(data)
+      console.log(data);
       if (response.ok) {
-        console.log(response.json)
-        setAlert({ type: 'success', message: 'Contact submitted successfully!' });
+        console.log(response.json);
+        setAlert({
+          type: 'success',
+          message: 'Contact submitted successfully!',
+        });
       } else {
         const errorData = await response.json();
-        setAlert({ type: 'error', message: errorData.message || 'Submission failed.' });
+        setAlert({
+          type: 'error',
+          message: errorData.message || 'Submission failed.',
+        });
       }
     } catch (error) {
-        console.log(error)
+      console.log(error);
       setAlert({ type: 'error', message: 'An unexpected error occurred.' });
     } finally {
       setLoading(false);
@@ -50,8 +61,9 @@ export const ContactProvider = ({ children }) => {
   };
 
   return (
-    <ContactContext.Provider value={{ loading, alert, submitContact, setAlert }}>
-        
+    <ContactContext.Provider
+      value={{ loading, alert, submitContact, setAlert }}
+    >
       {children}
     </ContactContext.Provider>
   );
